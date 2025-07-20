6e5129233514010dea82e38e93fae90296f9d2d1
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-07-20 09:44:03 +0530

    connection change

--- components/api.php
+++ components/api.php
@@ -5,8 +5,8 @@
 
 // $img_path="http://localhost/sugan/api/";
 
-$API="http://admin.sugantourism.com/api/routes/";
+$API="https://www.admin.sugantourism.com/api/routes/";
 
-$img_path="http://admin.sugantourism.com/login.php/api/";
+$img_path="https://www.admin.sugantourism.com/api/";
 
 ?>
